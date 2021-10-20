678b92f2dcd43376c11975066bdc0170574b095d
Hitesh932 <71250840+Hitesh932@users.noreply.github.com>
2021-10-20 20:21:36 +0530

    Add files via upload

---
+++ ConvertController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CreateBitmap.Controllers
+{
+    public class ConvertController : Controller
+    {
+        // GET: Convert
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public void GetBitmap()
+        {
+            var text = "";
+            string alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string small_alphabets = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "1234567890";
+
+            string characters = numbers;
+
+            characters += alphabets + small_alphabets + numbers;
+            string otp = string.Empty;
+            for (int i = 0; i < 6; i++)
+            {
+                string character = string.Empty;
+                do
+                {
+                    int index = new Random().Next(0, characters.Length);
+                    character = characters.ToCharArray()[index].ToString();
+                } while (otp.IndexOf(character) != -1);
+                otp += character;
+            }
+            text = otp.ToString();
+
+            var path = "";
+            Bitmap bitmap = new Bitmap(1, 1);
+            Font font = new Font("Arial", 25, FontStyle.Regular, GraphicsUnit.Pixel);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            int width = (int)graphics.MeasureString(text, font).Width;
+            int height = (int)graphics.MeasureString(text, font).Height;
+            bitmap = new Bitmap(bitmap, new Size(width, height));
+            graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.White);
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+            graphics.DrawString(text, font, new SolidBrush(Color.FromArgb(255, 0, 0)), 0, 0);
+            graphics.Flush();
+            graphics.Dispose();
+            string fileName = Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".bmp";
+            bitmap.Save(Server.MapPath("~/images/") + fileName, ImageFormat.Bmp);
+            path = "~/images/" + fileName;
+            
+        }
+      
+    }
+    
+}
